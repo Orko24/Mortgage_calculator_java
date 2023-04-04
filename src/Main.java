a4a0159bed587a9aee6b9d426f9e8c5b332e0285
Orko15 <50932841+Orko15@users.noreply.github.com>
2023-04-04 01:04:30 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principle: ");
+        System.out.print("Please enter a Principle between 1,000 and 1,000,000: ");
         long Principle = scanner.nextLong();
 
-        System.out.print("Annual Interest Rate: ");
-        double Annual_Interest_Rate = scanner.nextDouble()/100;
+        while (Principle < 1000 || Principle > 1_000_000){
 
-        System.out.print("Period (Years): ");
+            System.out.print("Please enter a Principle between 1,000 and 1,000,000: ");
+            Principle = scanner.nextLong();
+        }
+
+        System.out.print("Please enter an Annual Interest Rate between 0 and 30: ");
+        double Annual_Interest_Rate = scanner.nextDouble();
+
+        while (Annual_Interest_Rate < 0 || Annual_Interest_Rate > 30){
+            System.out.print("Please enter an Annual Interest Rate between 0 and 30: ");
+            Annual_Interest_Rate = scanner.nextDouble();
+        }
+
+        Annual_Interest_Rate = Annual_Interest_Rate/100;
+
+        System.out.print("Please enter a Period (Years) between 1 and 30: ");
         long Period = scanner.nextLong();
 
-        //Varialbles:
-//        M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
-//
-//        Here’s a breakdown of each of the variables:
-//
-//        M = Total monthly payment
-//        P = The total amount of your loan
-//        I = Your interest rate, as a monthly percentage
-//        N = The total amount of months in your timeline for paying off your mortgage
+        while (Period < 1 || Period > 30){
+            System.out.print("Please enter a Period (Years) between 1 and 30: ");
+            Period = scanner.nextLong();
+        }
+
 
         double monthly_interest = Annual_Interest_Rate/12;
         long total_months = Period * 12;
